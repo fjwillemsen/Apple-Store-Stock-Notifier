aa82552f2dcadde0bbf5e3f85ed1250ffea036b8
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2024-09-18 16:51:58 -0700

    Implemented file watchers, auto-update and callbacks for config files

--- confighandler.py
+++ confighandler.py
@@ -1,22 +1,61 @@
 """Module for handling the configuration file."""
 
+import asyncio
 from pathlib import Path
 from tomlkit import load, dumps
+from watchfiles import awatch
 
 
 class ConfigHandler:
     """Class for handling configuration file reading and manipulation."""
 
     def __init__(self) -> None:
-        self.configfile = Path("config.toml")
+        """Initialization function. Note that you must listen for file changes by calling `watch_changes`."""
+        self.configfile_path = Path("config.toml")
         self.read()
+        print("Ready!")
+
+    def __get_contents_on_disk(self):
+        with self.configfile_path.open() as fp:
+            return load(fp)
 
     def read(self):
-        with self.configfile.open() as fp:
-            self.doc = load(fp)
+        print("read")
+        self.doc = self.__get_contents_on_disk()
 
     def write(self):
         dumps(self.doc)
+
+    def is_outdated(self) -> bool:
+        """Checks if the in-memory doc is outdated compared to the on-disk file."""
+        return self.__get_contents_on_disk() != self.doc
+
+    async def watch_changes(self, callback_on_external_update=None, auto_update=True):
+        """_summary_
+
+        Args:
+            callback_on_external_update (Callable, optional): callback function that is called after the configuration has been updated by another instance. Defaults to None.
+            auto_update (bool, optional): whether to automatically keep the in-memory doc up to date with the on-disk config. Defaults to True.
+        """
+        """Watch for changes to the config file, call callback_on_update if"""
+        async for _ in awatch(self.configfile_path, poll_delay_ms=1000):
+            if self.is_outdated():
+                print("outdated")
+                if auto_update:
+                    self.read()
+                if callback_on_external_update is not None:
+                    print("calling callback")
+                    callback_on_external_update()
+
+
+if __name__ == "__main__":
+
+    def hi():
+        print("hi there!")
+
+    ch = ConfigHandler()
+    asyncio.run(ch.watch_changes(hi, auto_update=False))
+    print("Ready!")
 
 
 # content = """[table]
